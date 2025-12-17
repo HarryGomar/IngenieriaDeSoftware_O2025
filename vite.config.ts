b9cd547952d2211fb3e9b25f317d917e8ca1e5a3
HarryGomar <rhgomar@gmail.com>
2025-12-16 20:02:49 -0600

    Path Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/IngenieriaDeSoftware_O2025/", // <--- MUST match your repo name exactly
 })
